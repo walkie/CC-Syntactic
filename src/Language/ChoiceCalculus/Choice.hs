c54c0e79ea699269bdc649022a1ffe0840012c7f
Eric Walkingshaw <eric@walkingshaw.net>
2013-12-10 16:33:18 +0100

    Generalize types.

--- src/Language/ChoiceCalculus/Choice.hs
+++ src/Language/ChoiceCalculus/Choice.hs
@@ -36,10 +36,10 @@ freeDims (Sym s)
   | Just (Chc2 d) <- prj s = Set.singleton d
   | otherwise              = Set.empty
 
-type V a = ASTF (Chc2 :+: One a) a
+type V a = (Chc2 :<: l, One Int :<: l) => ASTF l Int
 
-vint :: V Int
+vint :: V a
 vint = chc2 "A" (chc2 "B" (one 1) (one 2)) (chc2 "B" (one 3) (one 4))
 
--- vtree :: ASTF (Tree Int :+: Chc2 :+: One) (Tree Int t)
--- vtree = node (one 0) (cons (node vint nil) nil)
+vtree :: ASTF (Tree Int :+: List (Tree Int t) :+: Chc2 :+: One Int) (Tree Int t)
+vtree = node (one 0) (cons (node vint nil) nil)
